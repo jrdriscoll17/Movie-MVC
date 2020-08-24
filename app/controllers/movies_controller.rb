0349a59ff92a698c181fafcb79dda6e249f2651f
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-08-24 16:27:48 -0500

    Adds basic log in/sign up functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,11 @@
 class MoviesController < ApplicationController
+    get '/movies' do
+        if !logged_in?
+            @movies = Movies.all
 
-  end
+            erb :'movies/index'
+        else
+            redirect '/login'
+        end
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,25 @@ class UsersController < ApplicationController
         erb :'users/show'
     end
 
+    get '/signup' do
+        if !logged_in?
+            erb :'users/new', locals: {message: "Create an account to start your movie collection!"}
+        else
+            redirect to '/movies'
+        end
+    end
+
+    post '/signup' do
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+        erb :view
+    end
+
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to '/movies'
+        end
+    end
 end
