491d4d12302fe46854d785495bc18e890bb8c23d
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-08-24 17:41:42 -0500

    Adds log out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
         else
             @user = User.new(username: params[:username], email: params[:email], password: params[:password])
             @user.save
-            
+
             session[:user_id] = @user.id
 
             redirect to '/movies'
@@ -31,6 +31,25 @@ class UsersController < ApplicationController
             erb :'users/login'
         else
             redirect to '/movies'
+        end
+    end
+
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user #&& user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to "/movies"
+        else
+          redirect to '/signup'
+        end
+      end
+    
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect to '/login'
+        else
+            redirect to '/'
         end
     end
 end
