9c5f800013430de4454551e0fa24c2cfbee7ed9f
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-08-25 19:33:52 -0500

    Adds index view for genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,3 +1,18 @@
 class GenresController < ApplicationController
+    get '/genres' do
+        if logged_in?
+            @genres = Genre.all
+
+            erb :'genres/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/genres/:id' do
+        @genre = Genre.find_by_id(params[:id])
+        
+        erb :'/genres/view'
+    end
 
 end
