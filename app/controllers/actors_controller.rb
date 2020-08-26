2068f87d722c9d3b91321ea2cc50df3c63c19fc5
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-08-25 20:11:11 -0500

    Adds index view for actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,17 @@
 class ActorsController < ApplicationController
-    
+    get '/actors' do
+        if logged_in?
+            @actors = Actor.all
+
+            erb :'actors/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/actors/:id' do
+        @actor = Actor.find_by_id(params[:id])
+        
+        erb :'/actors/view'
+    end
 end
